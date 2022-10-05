7e9396876de9c6d8b9c5e83980641ce91bac3743
lucasrenandns <lucasrenandns96@gmail.com>
2022-10-04 21:39:15 -0300

    Update js

--- js/typing.js
+++ js/typing.js
@@ -1,9 +1,9 @@
-const paragraph = document.querySelector("#text")
+const paragraph = document.getElementById("text")
 const text = paragraph.innerHTML.split("")
 
 function typingEffect(elemento) {
-
     elemento.innerHTML = ""
+
     for(let i = 0; i < text.length; i++) {
         setTimeout(function() {
             elemento.innerHTML += text[i]
